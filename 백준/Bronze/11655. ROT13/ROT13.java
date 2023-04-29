f37abfc5162471a0be44a44bec1fba7ae8ab486d
strongmhk <kim6562166086@gmail.com>
2023-04-29 23:24:00 +0900

    Optimize ROT13.java

--- 백준/Bronze/11655. ROT13/ROT13.java
+++ 백준/Bronze/11655. ROT13/ROT13.java
@@ -9,21 +9,14 @@ public class Main {
 
         String s = br.readLine();
         StringBuilder str = new StringBuilder("");
+        
         for(int i = 0; i < s.length(); i++){
             char ch = s.charAt(i);
             int num = (int)ch;
-            if((num >= 65 && num <= 77) || (num >= 97 && num <= 109)){
-                ch = (char)(num + 13);
-                str.append(ch);
-            }
-            if((num >= 78 && num <= 90) || (num >= 110 && num <= 122)){
-                ch = (char)(num - 13);
-                str.append(ch);
-            }
-            if((num >= 48 && num <= 57) || num == 32) str.append(ch);
-
+            if((num >= 65 && num <= 77) || (num >= 97 && num <= 109)) ch = (char)(num + 13);
+            if((num >= 78 && num <= 90) || (num >= 110 && num <= 122)) ch = (char)(num - 13);
+            str.append(ch);
         }
         System.out.println(str);
-
     }
 }
