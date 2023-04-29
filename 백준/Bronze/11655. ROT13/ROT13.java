e7eb3d870b60325e47bfda1b168a0119cc928f7e
strongmhk <kim6562166086@gmail.com>
2023-04-29 23:11:57 +0900

    [Bronze I] Title: ROT13, Time: 124 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Bronze/11655. ROT13/ROT13.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.lang.StringBuilder;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+        StringBuilder str = new StringBuilder("");
+        for(int i = 0; i < s.length(); i++){
+            char ch = s.charAt(i);
+            int num = (int)ch;
+            if((num >= 65 && num <= 77) || (num >= 97 && num <= 109)){
+                ch = (char)(num + 13);
+                str.append(ch);
+            }
+            if((num >= 78 && num <= 90) || (num >= 110 && num <= 122)){
+                ch = (char)(num - 13);
+                str.append(ch);
+            }
+            if((num >= 48 && num <= 57) || num == 32) str.append(ch);
+
+        }
+        System.out.println(str);
+
+    }
+}
