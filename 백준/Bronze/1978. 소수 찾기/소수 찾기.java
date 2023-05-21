24f8479220871672f0689070415a64d9d00ab222
strongmhk <kim6562166086@gmail.com>
2023-05-21 14:18:08 +0900

    [Bronze II] Title: 소수 찾기, Time: 124 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.java
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -12,10 +12,10 @@ public class Main {
         for (int i = 0; i < N; i++) {
             int count = 0;
             int k = Integer.parseInt(nums[i]);
-            for (int j = 1; j <= k; j++) {
+            for (int j = 1; j <= k; j++) { // 숫자를 1부터 자기 자신까지 나눠주기
                if(k%j == 0) count++;
             }
-            if(count == 2) prime++;
+            if(count == 2) prime++; // 1과 자기 자신의 숫자로만 나누어 떨어지면 소수
         }
 
         System.out.println(prime);
