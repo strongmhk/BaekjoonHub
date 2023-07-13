93f487d3abedc6a959ecacdde3a053c66a2946e9
strongmhk <kim6562166086@gmail.com>
2023-07-14 08:06:38 +0900

    [Gold V] Title: 평범한 배낭, Time: 192 ms, Memory: 53924 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[] weights = new int[N + 1];
+        int[] values = new int[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            weights[i] = Integer.parseInt(st.nextToken());
+            values[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[][] dp = new int[N + 1][K + 1];
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= K; j++) {
+                if (weights[i] <= j) {
+                    dp[i][j] = Math.max(dp[i - 1][j], values[i] + dp[i - 1][j - weights[i]]);
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+
+        System.out.println(dp[N][K]);
+    }
+}
