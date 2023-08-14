0b1f57737dadf3b7598f6919ddca5de11b459d13
strongmhk <kim6562166086@gmail.com>
2023-08-15 00:02:01 +0900

    [Silver III] Title: 소수 구하기, Time: 816 ms, Memory: 29000 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] input = br.readLine().split(" ");
+        int M = Integer.parseInt(input[0]);
+        int N = Integer.parseInt(input[1]);
+
+        boolean[] isPrime = new boolean[N + 1];
+        for (int i = 2; i <= N; i++) {
+            isPrime[i] = true;
+        }
+
+        for (int i = 2; i * i <= N; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j <= N; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        ArrayList<Integer> primes = new ArrayList<>();
+        for (int i = M; i <= N; i++) {
+            if (isPrime[i]) {
+                primes.add(i);
+            }
+        }
+
+        for (int prime : primes) {
+            System.out.println(prime);
+        }
+    }
+}
