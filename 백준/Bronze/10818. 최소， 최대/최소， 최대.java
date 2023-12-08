537f60c91a5d215a75d7ce95e5a991feb274c7f2
strongmhk <kim6562166086@gmail.com>
2023-12-08 20:18:04 +0900

    [Bronze III] Title: 최소, 최대, Time: 1328 ms, Memory: 104248 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] nums = new int[N];
+
+        st = new StringTokenizer(br.readLine(), " ");
+
+        for (int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(nums);
+
+        sb.append(nums[0] + " ").append(nums[N-1]);
+
+        System.out.println(sb);
+
+    }
+}
