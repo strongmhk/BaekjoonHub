9bb2152bedc6f500701a2970acef2f96e8613622
strongmhk <kim6562166086@gmail.com>
2025-04-05 22:40:37 +0900

    [Bronze V] Title: 직사각형, Time: 104 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+        
+        br.close();
+        
+        System.out.println(A*B);
+    }
+}
