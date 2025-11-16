cc3984f35bbd4006acfeed81be1467ca7985482f
strongmhk <kim6562166086@gmail.com>
2025-11-17 01:11:10 +0900

    [Recursive] 이진수 출력(재귀함수 이용)

---
+++ 인프런/7. Recursive, Tree, Graph/2. 이진수 출력(재귀함수 이용)/Main.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    static void recursive(int N) {
+        if (N == 0) return;
+        else {
+            recursive(N / 2);
+            System.out.print(N % 2);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        recursive(N);
+    }
+}
