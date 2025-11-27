c9bb0c0e9b6d25b21c0610e0e3737a051bd2b48b
strongmhk <kim6562166086@gmail.com>
2025-11-27 23:47:38 +0900

    [DFS] 경로탐색

---
+++ 인프런/7. Recursive, Tree, Graph/12. 경로탐색(DFS)/Main.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    static int[][] graph;
+    static int N, M, answer = 0;
+    static int[] visit;
+
+    public void DFS(int v) {
+        if(v == N) return answer++;
+        for(int i = 1; i <= N; i++) {
+            if(graph[v][i] == 1 && visit[i] == 0) {
+                visit[i] = 1;
+                DFS(i);
+                visit[i] = 0;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        N = Integer.parseInt(st.nextToken(), 10);
+        M = Integer.parseInt(st.nextToken(), 10);
+
+        graph = new int[N+1][N+1];
+        visit = new int[N+1];
+
+        for(int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken(), 10);
+            int b = Integer.parseInt(st.nextToken(), 10);
+
+            graph[a][b] = 1;
+        }
+
+        visit[1] = 1;
+
+        DFS(1);
+        System.out.println(answer);
+    }
+}
