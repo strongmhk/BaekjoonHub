9260a646db1c9fcfa6b2eccaefa4d0cc98cc7640
strongmhk <kim6562166086@gmail.com>
2026-05-07 23:13:29 +0900

    [level 1] Title: 가장 많이 받은 선물 - 2번째 풀이

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
@@ -2,55 +2,56 @@ import java.util.*;
 
 class Solution {
     public int solution(String[] friends, String[] gifts) {
-        int answer = 0;
-        
-        int n = friends.length;
-        
-        int[] giftPoint = new int[n];
-        int[][] giftGraph = new int[n][n];
-        
-        // (친구 : 배열 인덱스) map 생성
-        Map<String, Integer> friendsMap = new HashMap<>();
-        
-        for (int i = 0; i < n; i++) {
+        // 사람이름 : 행렬 인덱스 매핑
+        HashMap<String, Integer> friendsMap = new HashMap<>();
+
+        for (int i = 0; i < len; i++) {
             friendsMap.put(friends[i], i);
         }
-        
-        for (String gift : gifts) {
-            String[] arr = gift.split(" ");
-            String giver = arr[0];
-            String receiver = arr[1];
-            
-            // 선물 그래프 갱신
+
+        int n = friends.length;
+        int[][] giftGraph = new int[n][n];
+        int[] giftDegree = new int[n];
+
+        // 인접 행렬, 선물 지수 계산
+        for (String giftHist : gifts) {
+            String[] split = giftHist.split(" ");
+            String giver = split[0];
+            String receiver = split[1];
+
             int giverIdx = friendsMap.get(giver);
             int receiverIdx = friendsMap.get(receiver);
+
+            // 인접 행렬 계산
             giftGraph[giverIdx][receiverIdx]++;
-            
-            // 선물 지수 증/감 처리
-            giftPoint[giverIdx]++;
-            giftPoint[receiverIdx]--;
+
+            // 선물 지수 계산
+            giftDegree[giverIdx]++;
+            giftDegree[receiverIdx]--;
         }
-        
+
+        // 최댓값 계산
+        int answer = 0;
+
         for (int i = 0; i < n; i++) {
             int giftCnt = 0;
-            
+
             for (int j = 0; j < n; j++) {
                 if (i == j) continue;
-                
-                // 두 사람이 선물을 주고 받은 적이 있다면 -> a가 b한테 받은 선물보다 준 선물이 더 많을 때 b -> a한테 선물 줘야함
+                // 선물을 주고받은 기록이 있는 경우
                 if (giftGraph[i][j] != 0 || giftGraph[j][i] != 0) {
+                    // 더 많이 준 사람이 선물을 받음
                     if (giftGraph[i][j] > giftGraph[j][i]) giftCnt++;
-                } 
-                // 두 사람이 선물을 주고 받은 적이 없거나, 같은 수로 주고 받았다면
-                // 선물 지수가 더 큰 사람이 작은 사람에게 받음
-                if (giftGraph[i][j] == 0 && giftGraph[j][i] == 0 || giftGraph[i][j] == giftGraph[j][i]) {
-                    if (giftPoint[i] > giftPoint[j]) giftCnt++;
+                    // 선물을 주고받은 기록이 없거나 같은 수의 선물을 주고받았을 경우
+                } else if ((giftGraph[i][j] == 0 && giftGraph[j][i] == 0) || giftGraph[i][j] == giftGraph[j][i]) {
+                    // 선물 지수가 높은 사람이 선물을 받음
+                    if (giftDegree[i] > giftDegree[j]) giftCnt++;
                 }
             }
-            
+
             answer = Math.max(answer, giftCnt);
         }
-        
+
         return answer;
     }
 }
