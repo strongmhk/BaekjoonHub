0977ad79a7e4e85f5e71219c4b67e6dec3329cc7
strongmhk <kim6562166086@gmail.com>
2026-08-02 01:17:26 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.12 ms, Memory: 57.7 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -2,39 +2,39 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] maps) {
-        return BFS(maps, maps.length, maps[0].length);
+        return bfs(maps);
     }
-
-    static int BFS(int[][] maps, int n, int m) {
-        // 큐에 {x좌표, y좌표, 이동거리} 형태의 배열을 넣기
+    
+    static int bfs(int[][] maps) {
+        int n = maps.length;
+        int m = maps[0].length;
+        
+        int[] dx = {-1, 0, 1, 0};
+        int[] dy = {0, 1, 0, -1};
+        
         boolean[][] visited = new boolean[n][m];
         Queue<int[]> queue = new LinkedList<>();
-        int[] dx = {-1, 0, 1, 0}; // 상, 우, 하, 좌
-        int[] dy = {0, 1, 0, -1};
-        int result = -1;
-
-        // 시작 좌표 큐에 넣고 방문 처리
-        queue.offer(new int[]{0, 0, 1});
-        visited[0][0] = true;
-
-        while(!queue.isEmpty()) {
+        
+        queue.add(new int[]{0, 0, 1});
+        
+        while (!queue.isEmpty()) {
             int[] cur = queue.poll();
             int x = cur[0], y = cur[1], dis = cur[2];
-
+            
             if (x == n - 1 && y == m - 1) return dis;
-
+            
             for (int i = 0; i < 4; i++) {
                 int nx = x + dx[i];
                 int ny = y + dy[i];
-
+                
                 if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
                 if (maps[nx][ny] == 0 || visited[nx][ny] == true) continue;
-
-                queue.offer(new int[]{nx, ny, dis + 1});
+                
                 visited[nx][ny] = true;
+                queue.offer(new int[]{nx, ny, dis + 1});  
             }
         }
-
+        
         return -1;
     }
 }
