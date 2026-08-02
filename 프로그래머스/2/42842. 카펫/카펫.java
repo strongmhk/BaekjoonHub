056763be68628b4ac02eecbd802fdb359d41d34d
strongmhk <kim6562166086@gmail.com>
2026-08-02 11:48:44 +0900

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 83.5 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,26 +1,21 @@
 class Solution {
-
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
+        int total = brown + yellow;
         
-        int sum = brown + yellow;
-        
-        for (int i = 3; i * i <= sum; i++) {
-            if (sum % i == 0 && check(brown, yellow, sum / i, i)) {
-                answer[0] = sum / i; // 더 큰 값(몫) -> 가로
-                answer[1] = i; // 더 작은 값(나누는 수) -> 세로
-                break;
+        for (int height = 3; height * height <= total; height++) {
+            if (total % height == 0) {
+                int width = total / height;
+                if ((width - 2) * (height - 2) == yellow) {
+                    answer[0] = width;
+                    answer[1] = height;
+                }
             }
         }
-
+        
         return answer;
     }
-    
-    static boolean check(int brown, int yellow, int width, int height) {
-        int targetBrownCnt = width * 2 + ((height - 2) * 2);
-        int targetYellowCnt = (width - 2) * (height - 2);
-        
-        if (targetBrownCnt == brown && targetYellowCnt == yellow) return true;
-        return false;
-    }
 }
+
+// 1. 블럭 개수의 총합에 대한 약수를 구한다.(가로, 세로 너비 후보 구하기)
+// 2. 각 조합에 대해 노란색 블럭 개수를 계산하고 yellow랑 맞는지 확인한다.
