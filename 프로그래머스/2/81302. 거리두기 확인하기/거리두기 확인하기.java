fe763b9dff0b26636721ee10d8e9fdaf82242f75
strongmhk <kim6562166086@gmail.com>
2026-08-05 22:12:04 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.06 ms, Memory: 82 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -1,59 +1,58 @@
-import java.util.*;
-
 class Solution {
-    static int[] dr = {-1, 1, 0, 0};
-    static int[] dc = {0, 0, -1, 1};
-
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    
     public int[] solution(String[][] places) {
-        int[] answer = new int[places.length];
+        int[] answer = new int[5];
         
         for (int i = 0; i < 5; i++) {
-            String[] p = places[i];
+            String[] place = places[i];
             
-            boolean isOk = true;
-            for (int r = 0; r < 5 && isOk == true; r++) {
-                for (int c = 0; c < 5 && isOk == true; c++) {
-                    // 모든 자리 탐색할 필요 X, 사람있는 곳에서만 탐색 시작
-                    if (p[r].charAt(c) == 'P') {
-                        if (BFS(r, c, p) == false) isOk = false;
-                    }
-                }
-            }
+            boolean flag = isOK(place);
             
-            answer[i] = (isOk == true) ? 1 : 0;
+            if (flag) answer[i] = 1;
+            else answer[i] = 0;
         }
         
         return answer;
     }
     
-    static boolean BFS(int r, int c, String[] p) {
-        
-        
-        Queue<int[]> queue = new LinkedList<>();
-        boolean[][] visited = new boolean[5][5];
-        
-        queue.offer(new int[] {r, c});
-        visited[r][c] = true;
-        
-        while (!queue.isEmpty()) {
-            int[] cur = queue.poll();
-            
-            for (int i = 0; i < 4; i++) {
-                int nr = cur[0] + dr[i];
-                int nc = cur[1] + dc[i];
-            
-                if (nr < 0 || nr >= 5 || nc < 0 || nc >= 5 || (nr == r && nc == c)) continue;
-                if (visited[nr][nc] == true) continue;
-                
-                int d = Math.abs(nr - r) + Math.abs(nc - c);
-                if (d > 2) continue;
-                
-                visited[nr][nc] = true;
-                
-                if (p[nr].charAt(nc) == 'P' && d <= 2) return false;
-                else if (p[nr].charAt(nc) == 'O' && d < 2) queue.offer(new int[] {nr, nc});
+    boolean isOK(String[] place) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (place[i].charAt(j) == 'P') {
+                    // 맨해튼 거리 2사이에 뭐가 없는지 체크
+                    for (int k = 0; k < 4; k++) {
+                        int nx = i + dx[k];
+                        int ny = j + dy[k];
+                        
+                        if (nx < 0 || nx >= 5 || ny < 0 || ny >= 5) continue;
+                        
+                        int dist = Math.abs(i - nx) + Math.abs(j - ny);
+                        
+                        if (place[nx].charAt(ny) == 'P') {
+                            return false;
+                        } else if (place[nx].charAt(ny) == 'X') {
+                            continue;
+                        } else { // 붙어있는 자리가 빈자리일 경우 그 빈자리의 주변 탐색
+                            for (int l = 0; l < 4; l++) {
+                                int nx2 = nx + dx[l];
+                                int ny2 = ny + dy[l];
+                                
+                                if (nx2 < 0 || nx2 >= 5 || ny2 < 0 || ny2 >= 5) continue;
+                                if (nx2 == i && ny2 == j) continue;
+                                
+                                // 원래 자리의 주변 자리가 빈자리이면서, 그 빈자리의 주변에 사람이 있을 경우 거리두기 위반
+                                if (place[nx2].charAt(ny2) == 'P') return false;
+                            }
+                        }
+                    }
+                    
+                }
             }
         }
+        
+        // 위반하고 있는게 없으면 true
         return true;
     }
 }
