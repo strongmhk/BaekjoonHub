c7115d1db36a1006c74783a04b913837c09fc70d
strongmhk <kim6562166086@gmail.com>
2026-08-12 13:07:39 +0900

    [level 2] Title: 의상, Time: 2.36 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,16 +1,14 @@
 import java.util.*;
 
-public class Solution {
+class Solution {
     public int solution(String[][] clothes) {
-        int result = 1;
+        Map<String, Integer> map = new HashMap<>();
         
-        HashMap<String, Integer> map = new HashMap<>();
-        
-        for (String[] clothe : clothes) {
-            String type = clothe[1];
-            map.put(type, map.getOrDefault(type, 0) + 1);
+        for (String[] s : clothes) {
+            map.put(s[1], map.getOrDefault(s[1], 0) + 1);
         }
         
+        int result = 1;
         for (int count : map.values()) {
             result *= (count + 1);
         }
